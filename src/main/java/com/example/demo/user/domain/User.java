badc48957cd75e61012ba3dbcef768f8d826a994
yubin <dbqls2821@gmail.com>
2023-06-13 22:08:52 +0900

    feat: User Entity

--- src/main/java/com/example/demo/user/domain/User.java
+++ src/main/java/com/example/demo/user/domain/User.java
@@ -1,17 +1,32 @@
 package com.example.demo.user.domain;
 
+import com.example.demo.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
-@NoArgsConstructor
-public class User {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  @Embedded
+  private UserId userId;
 
+  private String name;
 
+  @Embedded
+  private Password password;
 
+  @Embedded
+  private Money money;
+
+  @Builder
+  public User(UserId userId, Password password, String name, Money money) {
+    this.userId = userId;
+    this.password = password;
+    this.name = name;
+    this.money = money;
+  }
 }
